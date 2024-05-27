104b2c944fc2107537cad3a1431af091ae8cf547
Nomex2008 <97978327+Nomex2008@users.noreply.github.com>
2024-05-27 13:52:47 +0300

    Async await
    Async await.

--- app.js
+++ app.js
@@ -35,3 +35,28 @@ fetch('https://jsonplaceholder.typicode.com/photos')
     .then((response) => {
         console.log(response)
     })
+
+async function getPosts(url) {
+    const response = await fetch(url)
+    const posts = await response.json()
+    return posts
+}
+
+getPosts('https://jsonplaceholder.typicode.com/photos')
+    .then((posts) => {
+        console.log(posts[0])
+    })
+    .catch(console.error)
+
+async function getPosts2(url) {
+    try {
+        const response = await fetch(url)
+        const posts = await response.json()
+        console.log(posts[1])
+    } catch (error) {
+        console.warn(error)
+        return error
+    }
+}
+    
+getPosts2('https://jsonplaceholder.typicode.com/photos')
